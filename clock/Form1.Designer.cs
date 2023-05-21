e355fe5375856bad1b88d1741377036b8a50a49e
Pachurina Darya <126852495+skyroro@users.noreply.github.com>
2023-05-21 18:28:33 +0300

    Add files via upload

---
+++ clock/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace mod_lab09_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1383, 883);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "clock";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ clock/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace mod_lab09_clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();    
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)//Главный код программы сосредоточен в функции-обработчике события
+        {
+            Graphics g = e.Graphics;
+            GraphicsState gs;
+            gs = g.Save();
+
+            int centerX = this.Width / 2;
+            int centerY = this.Height / 2;
+            int radius = 150;
+            int fontSize = 20;
+
+            // рисуем основу
+            Pen thinBlackPen = new Pen(Color.Black, 1);
+            Pen thickBlackPen = new Pen(Color.Black, 2);
+            SolidBrush brush = new SolidBrush(Color.FromArgb(128, 255, 255, 255));
+            g.FillEllipse(brush, centerX - radius, centerY - radius, 2*radius, 2 * radius);
+            g.DrawEllipse(thickBlackPen, centerX - radius, centerY - radius, 2*radius, 2*radius);
+            g.DrawEllipse(thickBlackPen, centerX, centerY, 4, 4);
+
+            // рисуем циферблат
+            for (int i = 1; i <= 12; i++)
+            {
+                double angle = i * Math.PI / 6 - Math.PI / 2;
+                int x = centerX + (int)((radius - 1.5 * fontSize) * Math.Cos(angle));
+                int y = centerY + (int)((radius - 1.5 * fontSize) * Math.Sin(angle));
+                float koeff = (float)(fontSize * 0.7);
+                g.DrawString(i.ToString(), new Font("Arial", fontSize), Brushes.Black, x - koeff, y - koeff);
+            }
+
+            // рисуем секундные деления
+            for (int i = 0; i <= 60; i++)
+            {
+                g.TranslateTransform(centerX, centerY);
+                g.RotateTransform(6 * i);
+                g.DrawLine(thinBlackPen, 0, -radius, 0, -radius + 7);
+                g.Restore(gs);
+                gs = g.Save();
+            }
+
+            // рисуем минутные деления
+            for (int i = 0; i < 12; i++)
+            {
+                g.TranslateTransform(centerX, centerY);
+                g.RotateTransform(30 * i);
+                g.DrawLine(thickBlackPen, 0, -radius, 0, -radius + 12);
+                g.Restore(gs);
+                gs = g.Save();
+            }
+
+            // рисуем стрелки
+            DateTime dt = DateTime.Now;
+            float secondAngle = 6 * dt.Second - 90;
+            float minuteAngle = 6 * (dt.Minute + (float)dt.Second / 60) - 90;
+            float hourAngle = 30 * (dt.Hour % 12) + dt.Minute / 2 - 90;
+
+            Pen secondPen = new Pen(Color.Red, 1);
+            Pen minutePen = new Pen(Color.Black, 2);
+            Pen hourPen = new Pen(Color.Black, 3);
+
+            g.TranslateTransform(centerX, centerY);
+
+            // часовая стрелка
+            gs = g.Save();
+            g.RotateTransform(30 * (dt.Hour + (float)dt.Minute / 60 + (float)dt.Second / 3600));
+            Point[] points1 = new Point[4];
+            points1[0] = new Point(0, 0);
+            points1[1] = new Point(-4, -20);
+            points1[2] = new Point(0, -radius + 60);
+            points1[3] = new Point(4, -20);
+            g.FillPolygon(Brushes.Black, points1);
+            g.Restore(gs);
+
+            // минутная стрелка
+            gs = g.Save();
+            g.RotateTransform(6 * (dt.Minute + (float)dt.Second / 60));
+            Point[] points2 = new Point[4];
+            points2[0] = new Point(0, 0);
+            points2[1] = new Point(-3, -20);
+            points2[2] = new Point(0, -radius + 30);
+            points2[3] = new Point(3, -20);
+            g.FillPolygon(Brushes.Black, points2);
+            g.Restore(gs);
+
+            // секундная стрелка
+            gs = g.Save();
+            g.RotateTransform(6 * (float)dt.Second);
+            g.DrawLine(secondPen, 0, 0, 0, -radius+10);
+            g.Restore(gs);          
+        }
+    }
+}
